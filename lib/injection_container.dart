ee40f73f9f7caa99d9cf0a7012b3394cd28e9907
Brayan <developergbp44@gmail.com>
2022-12-09 08:42:06 -0500

    my first commit

---
+++ lib/injection_container.dart
@@ -0,0 +1,23 @@
+import 'package:get_it/get_it.dart';
+
+final sl = GetIt.instance;
+
+Future<void> init() async {
+  //CONTROLLERS
+
+  //USE CASES
+
+  ///REPOSITORIES
+
+  ///DATA SOURCES
+
+  ///HIVE
+  //await Hive.initFlutter();
+
+  ///Adapters:
+  //Hive.registerAdapter(UserEntityAdapter());
+
+  ///Open boxes:
+  //await Hive.openBox<String>(HiveCollections.token);
+  //await Hive.openBox<AuthEntity>(HiveCollections.auth);
+}
---
+++ lib/main.dart
@@ -0,0 +1,6 @@
+import 'package:flutter/material.dart';
+import 'package:pruebas/src/features/app/app.dart';
+
+void main() {
+  runApp(const App());
+}
